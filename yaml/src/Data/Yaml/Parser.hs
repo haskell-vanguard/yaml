6ddc95684d12c32a8ae5d2a05577867d6b71a3e3
Fumiaki Kinoshita <fumiexcel@gmail.com>
2019-05-01 21:22:10 +0900

    Adapt to MonadFail

--- yaml/src/Data/Yaml/Parser.hs
+++ yaml/src/Data/Yaml/Parser.hs
@@ -8,6 +8,7 @@ module Data.Yaml.Parser where
 import Control.Applicative
 import Control.Exception (Exception)
 import Control.Monad (MonadPlus (..), liftM, ap)
+import qualified Control.Monad.Fail as Fail
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Resource (MonadThrow, throwM)
 import Control.Monad.Trans.Writer.Strict (tell, WriterT)
@@ -52,6 +53,10 @@ instance Monad YamlParser where
         case f am of
             Left t -> Left t
             Right x -> unYamlParser (g x) am
+#if !MIN_VERSION_base(4,13,0)
+    fail = Fail.fail
+#endif
+instance Fail.MonadFail YamlParser where
     fail = YamlParser . const . Left . pack
 instance MonadPlus YamlParser where
     mzero = fail "mzero"
